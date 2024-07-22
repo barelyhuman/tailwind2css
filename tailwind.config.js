3df9acd93dfd9eb935a7e8deb326a1069e3225c3
Reaper Gelera <ahoy@barelyhuman.dev>
2024-07-22 22:11:21 +0530

    initial impl

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        zincFoo: "#181819",
+      },
+    },
+  },
+  plugins: [],
+};
